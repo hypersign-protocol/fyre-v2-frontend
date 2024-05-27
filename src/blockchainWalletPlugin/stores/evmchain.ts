22a4ad830e99a43ee85b26032ec6013da69c8335
vishwas1 <vishwasanand.cse@gmail.com>
2024-05-27 18:00:17 +0530

    fixed wallet bug

--- src/blockchainWalletPlugin/stores/evmchain.ts
+++ src/blockchainWalletPlugin/stores/evmchain.ts
@@ -11,11 +11,23 @@ export const evmWalletsStore = defineStore('evmstore', {
         chainId: {},
         network: 'evm',
         connector: {}
-    }
+    },
+    hasSigningStarted: false, 
+    error: {
+      status: false,
+      message: "" 
+    },
   }),
   actions: {
     SET_EVM_RESULT (payload: any) {
         this.evmResultObject = {...payload}
+    },
+    SET_SIGNING_STARTED (payload: boolean) {
+      this.hasSigningStarted = payload
+    },
+    SET_ERROR (payload: any) {
+      this.error = {...payload}
     }
+
   }
 })
