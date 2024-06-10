3e4e0b10017f98658a24a2a8a02cbc55b0c82cb9
vishwas1 <vishwasanand.cse@gmail.com>
2024-06-10 18:40:24 +0530

    updated

--- src/components/Event/EventRewards/index.vue
+++ src/components/Event/EventRewards/index.vue
@@ -8,53 +8,80 @@
         <img class="modal__title__img ml-2" src="@/assets/images/gift.svg" height="24" />
       </div>
       <div class="modal__body">
-        <template v-for="(item, index) in eventData.rewards">
-          <div class="d-flex border__dotted pa-6 align-center justify-space-between">
-            <p class="font-16 font-weight-medium">Your Total Points</p>
-            <p class="font-22 font-weight-bold">{{ item.rewardPerPerson }} XP</p>
-          </div>
-          <div class="border__dotted pa-6 mt-5">
-            <div class="d-flex align-center justify-space-between mb-3">
-              <p class="font-16 font-weight-medium">Reward</p>
-              <p class="text-gradient-purple font-22 font-weight-bold">200 USTD</p>
+        <v-carousel show-arrows="hover" hide-delimiter-background>
+
+          <v-carousel-item v-for="(item, index) in eventData.rewards">
+
+            <div class=" pa-6 mt-5 border__dotted dynamic_background"
+              :style="{ 'background-image': 'url(' + getRewardTypeUrl(item.rewardType) + ')' }">
+              <div class=" d-flex align-center justify-space-between mb-3">
+                <p class="text-grey font-20 font-weight-bold">
+                  {{ item.title }}
+                </p>
+              </div>
+
+              <div class="d-flex align-center justify-space-between">
+                <p class="">
+                  <span class="text-gradient-blue font-22 font-weight-bold">{{ item.rewardPerPerson }}</span>&nbsp;
+                  <span class="text-white font-20 font-weight-bold">{{
+                    item.denomination
+                    }}</span><span class="font-15 font-weight-bold">/Winner</span>
+                </p>
+
+              </div>
+
+              <div class="d-flex align-center justify-space-between mb-4">
+                <p>
+                  <span class="text-green font-20 font-weight-bold">{{ item.winnerCount }}</span> <span
+                    class="text-white font-15 font-weight-bold">winners</span>
+                </p>
+              </div>
+
+              <div class="d-flex align-center justify-space-between mb-5">
+                <div class="row">
+                  <div class="col mb-1">
+                    <v-chip class=" mb-1" v-if="item.network">
+                      <img :src="getImage(item.network)" style="height: 20px;" /> <span class="ms-1">{{
+                        resolveConnectWalletTaskType(item.network)?.network }}</span>
+                    </v-chip>
+                    <v-chip class="mb-1 ms-1">
+                      <span class="ml-2">
+                        <v-icon icon="mdi-adjust" class="text-green"></v-icon>
+                        <span class="ms-1">Announcement on</span>
+                        <span class="text-blue ms-1">{{
+                          (new Date(eventData.endDate)).toDateString() }}
+                        </span>
+                      </span>
+                    </v-chip>
+                  </div>
+                </div>
+              </div>
+
+
             </div>
-            <div class="d-flex align-center justify-space-between mb-3">
-              <p class="font-16 font-weight-medium">Reward Per Winner</p>
-              <p class="text-gradient-blue font-22 font-weight-bold">
-                {{ item.rewardPerPerson }} USTD
-              </p>
-            </div>
-            <div class="d-flex align-center justify-space-between">
-              <p class="font-16 font-weight-medium">Winner Count</p>
-              <p class="text-gradient-green font-22 font-weight-bold">{{ item.winnerCount }}</p>
-            </div>
-          </div>
-          <div class="pa-6 mt-5">
-            <div class="d-flex align-center justify-space-between mb-3">
-              <p class="font-16 font-weight-medium">Winner Selection Method</p>
-              <p class="text-gray-100 font-14 font-weight-medium">{{ item.distributionType }}</p>
-            </div>
-            <div class="d-flex align-center justify-space-between mb-3">
-              <p class="font-16 font-weight-medium">Winner Announcement</p>
-              <p class="text-gray-100 font-14 font-weight-medium">After Uploading the list</p>
-            </div>
-            <div class="d-flex align-center justify-space-between">
-              <p class="font-16 font-weight-medium">Reward Distributed On</p>
-              <p class="text-gray-100 font-14 font-weight-medium">Ethereum</p>
-            </div>
-          </div>
-        </template>
-        <v-btn height="53" block class="claim--button">
-          <img src="@/assets/images/gift_open.svg" />
-          <span class="ml-2"> Claim Rewards</span>
-        </v-btn>
+            <v-btn height="53" block class="claim--button mt-3">
+              <img src="@/assets/images/gift_open.svg" />
+              <span class="ml-2"> Claim Rewards</span>
+            </v-btn>
+          </v-carousel-item>
+
+        </v-carousel>
       </div>
     </v-card>
   </v-dialog>
 </template>
+<style scoped>
+.dynamic_background {
+  background-position: right;
+  background-repeat:
+    no-repeat;
+  background-size: 182px 220px;
+  background-color: rgb(23 31 50 / 72%);
+}
+</style>
 <script lang="ts" setup>
 import { defineComponent, ref, onMounted, onBeforeUnmount, computed, watch } from 'vue'
-
+import { getImage, resolveConnectWalletTaskType } from '@/composables/event.ts'
 const props = defineProps({
   eventData: {
     type: Object,
@@ -64,6 +91,16 @@ const props = defineProps({
     }
   }
 })
+
+const getRewardTypeUrl = (rewardType: string) => {
+  if (rewardType === 'NFT') {
+    return "https://fyre.id/assets/nft-kozf1p3h.svg"
+  } else if (rewardType === 'TOKEN') {
+    return "https://fyre.id/assets/money-Cc8nUKC2.svg"
+  }
+
+  return "https://fyre.id/assets/nft-kozf1p3h.svg"
+}
 
 const dialog = ref(true)
 </script>
